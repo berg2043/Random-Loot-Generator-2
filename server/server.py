16603f06413188ec0d979be75464f21b2f71e70f
= <=>
2020-12-20 00:39:35 -0600

    home page now served at '/'

--- server/server.py
+++ server/server.py
@@ -3,9 +3,13 @@ import flask_restful
 from .routes.roll_route import RollRouter
 
 # Globals
-app = flask.Flask(__name__)
+app = flask.Flask(__name__,static_folder='./../public')
 api = flask_restful.Api(app)
 
 # Routes
 api.add_resource(RollRouter, '/api/roll')
+
 # Static
+@app.route('/')
+def home():
+  return app.send_static_file('index.html')
