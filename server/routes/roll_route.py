e17860a07eb51866523efeb334f63b25f1d27c01
= <=>
2020-12-20 00:35:43 -0600

    added route error handling

--- server/routes/roll_route.py
+++ server/routes/roll_route.py
@@ -1,8 +1,16 @@
 from flask_restful import Resource
 from flask import request
 from ..modules.Roller_JSON import roller
+import traceback
 
 
 class RollRouter(Resource):
   def get(self):
-    return roller(int(request.args["cr"]),request.args["group"])
+    try:
+      res = roller(int(request.args["cr"]),request.args["group"])
+      return res
+    except:
+      print('-------Roller Get Error---------')
+      traceback.print_exc()
+      print(request.args)
+      return {}, 500
