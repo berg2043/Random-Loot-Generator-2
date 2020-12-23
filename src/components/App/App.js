cb7ae1d2f09ec738a9b63ec807f7fd1dba1086c0
Phillip Berg <phillipb1991@msn.com>
2020-12-23 01:09:16 -0600

    added base form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="roll-container">
+        <div className="roll-item">
+          <form>
+            <label>CR</label>
+            <br/>
+            <input type="text"/>
+            <br/>
+            <label>Drop Method</label>
+            <br/>
+            <input type="radio" id="individual" name="group-size" defaultChecked/>
+            <label htmlFor="individual">Individual Monster</label>
+            <input type="radio" id="group" name="group-size"/>
+            <label htmlFor="group">Loot Horde</label>
+            <br/>
+            <button>Roll!</button>
+          </form>
+        </div>
+        <div className="roll-item"></div>
+      </div>
     </div>
   );
 }
