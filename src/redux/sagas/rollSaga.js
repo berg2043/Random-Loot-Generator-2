c22bd3239ee35749286d5bd9fbfa77393e096aa6
= <=>
2020-12-29 19:28:12 -0600

    added rollSaga to handle roll call

---
+++ src/redux/sagas/rollSaga.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { put, takeLatest, takeLeading } from 'redux-saga/effects';
+
+function* getRoll(action){
+  try {
+    const response = yield axios.get(
+      '/api/roll',
+      { 
+        params: {
+          cr: action.payload.challengeRating,
+          group: action.payload.lootType
+        }
+      }
+    );
+    yield put({type: 'SET_LOOT', payload: response.data});
+  } catch (error) {
+    console.log('Error eith GetRoll', error);
+  }
+}
+
+function* rollSaga(){
+  yield takeLatest('GET_ROLL', getRoll);
+}
+
+export default rollSaga;
