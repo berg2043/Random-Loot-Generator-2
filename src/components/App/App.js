9b575e8009f35ebe0c4219464c890522fbf54c4d
= <=>
2020-12-29 19:15:18 -0600

    added base function to handle input and test roll

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,43 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // States
+  const [cr, setCr] = useState(null);
+  const [group, setGroup] = useState('I');
+  
+  function roll(event){
+    event.preventDefault();
+    console.log(cr,group);
+  }
   return (
     <div className="App">
       <div className="roll-container">
         <div className="roll-item">
-          <form>
+          <form onSubmit={(event)=>{roll(event)}}>
             <label>CR</label>
             <br/>
-            <input type="text"/>
+            <input type="text" onChange={(event)=>{setCr(event.target.value)}}/>
             <br/>
             <label>Drop Method</label>
             <br/>
-            <input type="radio" id="individual" name="group-size" defaultChecked/>
+            <input 
+              type="radio" 
+              id="individual" 
+              name="group-size" 
+              checked={group === 'I'} 
+              value = 'I' 
+              onChange={()=>{setGroup('I')}}
+            />
             <label htmlFor="individual">Individual Monster</label>
-            <input type="radio" id="group" name="group-size"/>
+            <input 
+              type="radio" 
+              id="group" 
+              name="group-size" 
+              checked={group === 'G'} 
+              value = 'G' 
+              onChange={()=>{setGroup('G')}}
+            />
             <label htmlFor="group">Loot Horde</label>
             <br/>
             <button>Roll!</button>
