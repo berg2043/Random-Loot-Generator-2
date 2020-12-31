318806b93a7cf10f076984a9a7f69a700f1ecf59
= <=>
2020-12-31 14:34:28 -0600

    pulled input out into its own component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,63 +1,13 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
 import './App.css';
+import Input from './../Input/Input'
 import Loot from './../Loot/Loot';
 
 function App() {
-  // States
-  const [cr, setCr] = useState(null);
-  const [group, setGroup] = useState('I');
-  
-  // Setup Dispatch
-  const dispatch = useDispatch();
 
-  function roll(event){
-    event.preventDefault();
-    console.log(cr,group);
-    dispatch({
-      type: 'GET_ROLL',
-      payload: {
-        challengeRating: cr,
-        lootType: group
-      }
-    })
-  }
   return (
     <div className="App">
-      <div className="roll-container">
-        <div className="roll-item">
-          <form onSubmit={(event)=>{roll(event)}}>
-            <label>CR</label>
-            <br/>
-            <input type="text" onChange={(event)=>{setCr(event.target.value)}}/>
-            <br/>
-            <label>Drop Method</label>
-            <br/>
-            <input 
-              type="radio" 
-              id="individual" 
-              name="group-size" 
-              checked={group === 'I'} 
-              value = 'I' 
-              onChange={()=>{setGroup('I')}}
-            />
-            <label htmlFor="individual">Individual Monster</label>
-            <input 
-              type="radio" 
-              id="group" 
-              name="group-size" 
-              checked={group === 'G'} 
-              value = 'G' 
-              onChange={()=>{setGroup('G')}}
-            />
-            <label htmlFor="group">Loot Horde</label>
-            <br/>
-            <button>Roll!</button>
-          </form>
-        </div>
-        <Loot/>
-        <div className="roll-item"></div>
-      </div>
+      <Input/>
+      <Loot/>
     </div>
   );
 }
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,5 +1,59 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
 function Input() {
+  // States
+  const [cr, setCr] = useState(null);
+  const [group, setGroup] = useState('I');
+  
+  // Setup Dispatch
+  const dispatch = useDispatch();
+
+  // Calls saga to send challenge rating and loot type to server
+  function roll(event){
+    event.preventDefault();
+    console.log(cr,group);
+    dispatch({
+      type: 'GET_ROLL',
+      payload: {
+        challengeRating: cr,
+        lootType: group
+      }
+    })
+  }
+
   return (
-    
+    <div className="roll-item">
+      <form onSubmit={(event)=>{roll(event)}}>
+        <label>CR</label>
+        <br/>
+        <input type="text" onChange={(event)=>{setCr(event.target.value)}}/>
+        <br/>
+        <label>Drop Method</label>
+        <br/>
+        <input 
+          type="radio" 
+          id="individual" 
+          name="group-size" 
+          checked={group === 'I'} 
+          value = 'I' 
+          onChange={()=>{setGroup('I')}}
+        />
+        <label htmlFor="individual">Individual Monster</label>
+        <input 
+          type="radio" 
+          id="group" 
+          name="group-size" 
+          checked={group === 'G'} 
+          value = 'G' 
+          onChange={()=>{setGroup('G')}}
+        />
+        <label htmlFor="group">Loot Horde</label>
+        <br/>
+        <button>Roll!</button>
+      </form>
+    </div>
   )
 }
+
+export default Input;
